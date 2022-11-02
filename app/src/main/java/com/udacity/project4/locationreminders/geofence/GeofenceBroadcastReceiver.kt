688f2b99e622e4cbe115e90c91490823c212989d
EbramBafoor <abramibrahem22@gmail.com>
2022-11-02 19:49:59 +0200

    LocationReminder-FWD

---
+++ app/src/main/java/com/udacity/project4/locationreminders/geofence/GeofenceBroadcastReceiver.kt
@@ -0,0 +1,21 @@
+package com.udacity.project4.locationreminders.geofence
+
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import com.udacity.project4.locationreminders.savereminder.SaveReminderFragment.Companion.ACTION_GEOFENCE_EVENT
+
+
+
+class GeofenceBroadcastReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+
+        if (intent.action == ACTION_GEOFENCE_EVENT) {
+            Log.i("GeofenceReceiver", "Geofence event received")
+            GeofenceTransitionsJobIntentService.enqueueWork(context, intent)
+        }
+
+    }
+}
